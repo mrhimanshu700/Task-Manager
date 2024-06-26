b7cde1a709abcad759137b11a43b231220cfa034
mrhimanshu700 <156831028+mrhimanshu700@users.noreply.github.com>
2024-06-26 14:32:36 +0530

    Update utils.js

--- Task-Manager-App/frontend/src/utils.js
+++ Task-Manager-App/frontend/src/utils.js
@@ -1,6 +1,6 @@
 import { toast } from "react-toastify";
 
-export const API_URL = 'http://localhost:4000';
+export const API_URL = 'https://task-manager-api-swart.vercel.app';
 export const notify = (message, type) => {
     toast[type](message);
 }
